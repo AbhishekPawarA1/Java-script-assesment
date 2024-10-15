9ba13e76ca6c3ee9a5143c218e2115772c5fa75e
AbhishekA1 <abhishekanandpawar1@gmail.com>
2024-10-16 01:44:55 +0530

    Third-question

---
+++ assesment2/third-question.js
@@ -0,0 +1,20 @@
+
+function calculateTotalExpenses(expenses){
+    return expenses.reduce(function(emp,char){
+        return emp+char.amount
+    },0)
+}
+
+// Input
+const expenses = [
+    { name: "Food", amount: 50 },
+    { name: "Transportation", amount: 30 },
+    { name: "Internet", amount: 45 },
+    { name: "Groceries", amount: 80 }
+  ];
+  
+  // Output
+const totalExpenses = calculateTotalExpenses(expenses);
+console.log(totalExpenses); // Output should be: 205
+  
+  
